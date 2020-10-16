e67346ebd37bb5baa81daa6e3fc4edaa36b08b5e
sachin <sachinmohanfff@gmail.com>
2020-10-16 21:31:26 +0530

    forms added2

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -121,8 +121,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'new_static'),
-#)
+STATICFILES_DIRS = (
+ os.path.join(BASE_DIR, 'new_static'),
+
+)
