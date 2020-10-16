6f762d81f872d59d645ac7012cbd4401a538c89c
sachin <sachinmohanfff@gmail.com>
2020-10-16 21:49:34 +0530

    forms added5

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -118,12 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/blogapp/static/'
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
- os.path.join(BASE_DIR, 'blogapp/static'),
+ os.path.join(BASE_DIR, 'static'),
 
 )
